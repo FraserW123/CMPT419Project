46aed8ce28292d37f354a3e72a10793d2f106b17
frase <fgw@sfu.ca>
2025-03-28 13:49:08 -0700

    started the story

---
+++ story.py
@@ -0,0 +1,11 @@
+
+class Story():
+    def __init__(self):
+        self.done = False
+    
+    def introduction(self):
+        passage = "You're setting off on your own Pirate Adventure where exploration and treasures await! Somewhere beyond the horizon, a legendary treasure lies hidden, waiting to be claimed. But treasure hunting isn't just about sailing blindly into the unknown, you need information and strategy to outsmart the dangers that lurk ahead. " \
+        "With time running short, you must decide. Do you want to talk to townspeople for information or spy on rival pirates?"
+        
+        return passage
+    
